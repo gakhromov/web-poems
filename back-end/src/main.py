00954d2c8ac33fe33e6db1aead1bcf5d4045f497
Grigory Khromov <gakhromov@yandex.ru>
2021-03-07 16:28:38 +0300

    feat: added simple poem grading 🎓

--- back-end/src/main.py
+++ back-end/src/main.py
@@ -1,6 +1,6 @@
 from flask import Flask, request
 
-from poem_grader import get_poem_info 
+from poem_grader import get_poem_info, grade_poem
 
 
 app = Flask(__name__)
@@ -15,6 +15,16 @@ def hello_world():
 def poem_info():
     poem = request.json['poem']
     return get_poem_info(poem)
+
+
+@app.route('/grade-poem', methods=['POST'])
+def grade_poem_request():
+    if 'info' in request.json.keys():
+        info = request.json['info']
+    else:
+        poem = request.json['poem']
+        info = get_poem_info(poem)
+    return grade_poem(info)
 
 
 if __name__ == '__main__':
--- back-end/src/poem_grader.py
+++ back-end/src/poem_grader.py
@@ -3,6 +3,8 @@ import re
 import rusyllab
 import russtress
 
+import grading_rules
+
 
 ALPHABET = 'абвгдеёжзийклмнопрстуфхцчшщъыьэюя'
 STRONG_CONSONANTS = 'бвгджзклмнпрстфхцчшщъь'
@@ -25,11 +27,11 @@ def get_rhymes_from_punc_poem(puncless_poem):
     i = 0
     while i + 1 < len(syllab_list):
         if syllab_list[i] == syllab_list[i+1]:
-            rhymes[i+1] = {'rhymes_with': i}
+            rhymes[str(i+1)] = {'rhymes_with': str(i)}
         elif (i + 2 < len(syllab_list)) and (syllab_list[i] == syllab_list[i+2]):
-            rhymes[i+2] = {'rhymes_with': i}
+            rhymes[str(i+2)] = {'rhymes_with': str(i)}
         elif (i + 3 < len(syllab_list)) and (syllab_list[i] == syllab_list[i+3]):
-            rhymes[i+3] = {'rhymes_with': i}
+            rhymes[str(i+3)] = {'rhymes_with': str(i)}
         i = i + 1
     return rhymes
 
@@ -78,14 +80,41 @@ def get_poem_info(poem):
     punctuationless_poem = [re.sub(r'[^\w\s]', '', pm.lower()) for pm in poem]
     print(punctuationless_poem)
 
-    lines_info = get_rhymes_from_punc_poem(punctuationless_poem)
+    poem_info = get_rhymes_from_punc_poem(punctuationless_poem)
 
     accent = russtress.Accent()
 
     for i in range(len(punctuationless_poem)):
-        if i not in lines_info.keys():
-            lines_info[i] = dict()
+        if str(i) not in poem_info.keys():
+            poem_info[str(i)] = dict()
         stress_pattern = get_stress_pattern(accent, punctuationless_poem[i])
-        lines_info[i]['stress_pattern'] = check_stress_pattern(stress_pattern)
+        poem_info[str(i)]['stress_pattern'] = stress_pattern
+        poem_info[str(i)]['stress_pattern_stats'] = check_stress_pattern(stress_pattern)
 
-    return lines_info
+    return poem_info
+
+
+def grade_poem(poem_info):
+    points = dict()
+    lines_that_are_in_rhyme = set()
+    format_points = lambda x: f'+{x}' if x >= 0 else str(x)
+
+    for i in poem_info.keys():
+        points[i] = {'points': 0, 'comments': []}
+        if 'rhymes_with' in poem_info[i].keys():
+            points[i]['points'] += grading_rules.RHYMES_WITH_ONE_LINE
+            points[i]['comments'].append(
+                format_points(grading_rules.RHYMES_WITH_ONE_LINE) + ': Рифмуется с одной из предыдущих строк'
+            )
+            lines_that_are_in_rhyme.add(int(i))
+            lines_that_are_in_rhyme.add(int(poem_info[i]['rhymes_with']))
+    
+    for i in poem_info.keys():
+        if int(i) not in lines_that_are_in_rhyme:
+            points[i]['points'] += grading_rules.LINE_THAT_DOES_NOT_RHYME
+            points[i]['comments'].append(
+                format_points(grading_rules.LINE_THAT_DOES_NOT_RHYME) + ': Не рифмуется ни с одной из строк'
+            )
+
+    points['poem'] = sum([points[i]['points'] for i in points.keys()])
+    return points
