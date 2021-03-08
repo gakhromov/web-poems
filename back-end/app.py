cd19ebdba15f273cf754664493439c87056dbc58
Mikhail <mashulgin@edu.hse.ru>
2021-03-08 10:13:04 +0300

    (⁎⚈᷀᷁▿⚈᷀᷁⁎)

--- back-end/app.py
+++ back-end/app.py
@@ -2,13 +2,23 @@ import time
 import random
 
 from flask import Flask, request
+from flask_pymongo import PyMongo,pymongo
 import pusher
 
 from src.poem_grader import get_poem_info, grade_poem
 import src.vault
 
+import json
+from bson import json_util
 
 app = Flask(__name__)
+app.config['MONGO_URI'] = 'mongodb+srv://grishauser:grishapass@webpoemsmongo.nbpsd.mongodb.net/app?retryWrites=true&w=majority'
+
+mongo = PyMongo(app)
+leaderboard = mongo.db.leaderboard
+
+print(mongo.db.list_collection_names())
+
 pusher_client = pusher.Pusher(
   app_id='1167807',
   key='b45c83f8898eace14a49',
@@ -17,10 +27,47 @@ pusher_client = pusher.Pusher(
   ssl=True
 )
 
+#special json encoder for ObjectId BSON's fields
+def parse_json(data):
+    return json.loads(json_util.dumps(data))
+
+
+@app.route('/leaderboard', methods = ['GET'])
+def readFullLeaderBoard():
+    singlemode = request.json['single']
+    amount = request.json['amount']
+    try:
+        amount = int(amount)
+    except:
+        amount = 100
+
+    if singlemode:
+        result =  mongo.db.leaderboard.find({"playersamount":1},projection={"_id": 0})
+    else:
+        result =  mongo.db.leaderboard.find({"playersamount":{"$gt":1}},projection={"_id": 0})
+
+    result.sort('generalscore',pymongo.DESCENDING).limit(amount)
+
+    list_ = [doc for doc in result]
+
+    return {"leaderboard":list_}, 200
+
+
+
+
+
+
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+
+def postIntoDB(poem,userscores,generalscore):
+    leaderboard.insert_one({"userscores":userscores,
+                            "poem":poem,
+                            "generalscore":generalscore,
+                            "playersamount":len(userscores)})
 
 
 @app.route('/poem-info', methods=['POST'])
@@ -29,9 +76,13 @@ def poem_info():
     username = request.json['username']
     info = get_poem_info(poem)
     grades = grade_poem(info)
+    score = grades['poem']
+    rank = leaderboard.count_documents({"playersamount":1 ,'generalscore':{'$gt':score}}) + 1
+    postIntoDB(poem, [{'username':username,'score':score}], score)
     return {
         'info': info,
         'grades': grades,
+        'rank' : rank
     }, 200
 
 
@@ -55,6 +106,7 @@ def join_game():
         return {}, 400
     vault.sessions[session_id]['players'].append(username)
     return {}, 200
+
 
 
 @app.route('/game/players', methods=['GET'])
