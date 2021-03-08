efb8897c1dca48234204b2dfbfa3db6e16958db4
Maria <mdkurysheva@miem.hse.ru>
2021-03-08 18:43:50 +0300

    Send data to api

--- front-end/src/pages/Main/MainPage.js
+++ front-end/src/pages/Main/MainPage.js
@@ -1,33 +1,68 @@
 import React from 'react'
 import {Container, Form, Button} from "react-bootstrap";
 
-export const MainPage = () => {
-    const getResult = () => {
-        // console.log('123')
-        const requestOptions = {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*' },
-            body: JSON.stringify({ poem: 'React POST Request Example' })
-        }
-        fetch('http://localhost:9000/poems-info', requestOptions)
-            .then(response => response.json())
-            .then(response => console.log(response.json()))
+const Results = () => (
+    <div>
+        Some Results
+    </div>
+)
+
+export class MainPage extends React.Component {
+    state = {
+        showResults: false
     }
 
-    return (
-        <Container>
-            <Form>
-                <Form.Group controlId="exampleForm.ControlInput1">
-                    <Form.Label>Твое имя</Form.Label>
-                    <Form.Control type="email" placeholder="Мария" />
-                </Form.Group>
+    updateResultField = (data) => {
+        this.setState({ showResults: true })
+        console.log("data: ", data, "showResults: ", this.showResults)
+    }
 
-                <Form.Group controlId="exampleForm.ControlTextarea1">
-                    <Form.Label>Твой стих</Form.Label>
-                    <Form.Control as="textarea" rows={4} />
-                </Form.Group>
-            </Form>
-            <Button variant="primary" onClick={getResult}>Проверить стих</Button>
-        </Container>
-    )
+    getResult = () => {
+        const requestOptions = {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json'},
+            body: JSON.stringify({
+                poem: this.poem.value,
+                username: this.firstName.value,
+            })
+        }
+        fetch('http://localhost:9000/poem-info', requestOptions)
+            .then(async response => {
+                const data = await response.json();
+
+                if (!response.ok) {
+                    const error = (data && data.message) || response.status;
+                    return Promise.reject(error);
+                } else {
+                    this.updateResultField(data)
+                }
+            })
+            .catch(error => {
+                this.setState({ errorMessage: error.toString() });
+                console.error('There was an error!', error);
+            });
+    }
+
+    render() {
+        const res = (this.showResults) ? <Results /> : null
+        console.log(this.state, res)
+
+        return (
+            <Container>
+                <Form>
+                    <Form.Group controlId="exampleForm.ControlInput1">
+                        <Form.Label>Твое имя</Form.Label>
+                        <Form.Control type="text" placeholder="Мария" ref={(ref) => {this.firstName = ref}} />
+                    </Form.Group>
+
+                    <Form.Group controlId="exampleForm.ControlTextarea1">
+                        <Form.Label>Твой стих</Form.Label>
+                        <Form.Control as="textarea" rows={4} ref={(ref) => {this.poem = ref}} />
+                    </Form.Group>
+                </Form>
+                <Button variant="primary" onClick={this.getResult}>Проверить стих</Button>
+                { res }
+            </Container>
+        )
+    }
 }
