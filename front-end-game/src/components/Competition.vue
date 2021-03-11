6d5b4cc703148cb07d5bd7b81c92473acbad8e0f
Grigory Khromov <gakhromov@yandex.ru>
2021-03-12 00:03:49 +0300

    fix: fixed images sizes 🍆

--- front-end-game/src/components/Competition.vue
+++ front-end-game/src/components/Competition.vue
@@ -6,28 +6,28 @@
                 <h5>Игра-соревнование</h5><br>
                 <span v-for="(line, index) in poemLines" :key="index">
                     <div v-if="line.player == null" class="row">
-                        <div data-aos="fade-down" class="col-3"><p>Компьютер</p></div>
-                        <div data-aos="fade-down" class="col-9">{{line.line}}</div>
+                        <div data-aos="fade-right" class="col-3"><p>Компьютер</p></div>
+                        <div data-aos="fade-right" class="col-9">{{line.line}}</div>
                     </div>
                     <div v-else-if="typeof line.line === 'string'" class="row">
-                        <div data-aos="fade-down" class="col-3"><p>{{username}}</p></div>
-                        <div data-aos="fade-down" class="col-9">{{line.line}}</div>
+                        <div data-aos="fade-right" class="col-3"><p>{{username}}</p></div>
+                        <div data-aos="fade-right" class="col-9">{{line.line}}</div>
                     </div>
                     <div v-else></div>
                 </span>
                 <div v-if="poemLines[poemLines.length-2].line == null" class="row" style="margin-bottom: 10px">
                     <div data-aos="fade-right" class="col-3 text-right"><button @click="recordFirstLine" type="button" class="btn btn-success">✔</button></div>
-                    <div data-aos="fade-down" class="col-9"><input v-model="firstLine" type="text" class="form-control"/></div>
+                    <div data-aos="fade-right" class="col-9"><input v-model="firstLine" type="text" class="form-control"/></div>
                 </div>
                 <div v-if="poemLines[poemLines.length-1].line == null" class="row" style="margin-bottom: 10px">
                     <div data-aos="fade-right" v-if="typeof poemLines[poemLines.length-2].line === 'string'" class="col-3 text-right"><button @click="recordSecondLine" type="button" class="btn btn-success">✔</button></div>
                     <div data-aos="fade-right" v-else class="col-3 text-right"><button @click="recordSecondLine" type="button" disabled class="btn btn-success">✔</button></div>
-                    <div data-aos="fade-down" class="col-9"><input v-model="secondLine" type="text" class="form-control"/></div>
+                    <div data-aos="fade-right" class="col-9"><input v-model="secondLine" type="text" class="form-control"/></div>
                 </div>
                 <div v-if="gameFinished">
-                    <h4 data-aos="fade">Поздравляем, <b>соревнование</b> завершено!</h4>
-                    <label data-aos="fade-down">Ваш результат:</label><br>
-                    <label data-aos="fade-down" class="result-notice">Количество набранных баллов = <b>{{score}}</b>. <br>Место в лидерборде = <b>{{rank}}</b></label>
+                    <h4 data-aos="fade-right">Поздравляем, <b>соревнование</b> завершено!</h4>
+                    <label data-aos="fade-right">Ваш результат:</label><br>
+                    <label data-aos="fade-right" class="result-notice">Количество набранных баллов = <b>{{score}}</b>. <br>Место в лидерборде = <b>{{rank}}</b></label>
                 </div>
             </div>
             <div class="col-xl-3 col-lg-3 col-md-3 col-sm-12 d-none d-md-block">
